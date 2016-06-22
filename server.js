cca733f2fcae3dfbc0d47b317881980196a230f9
Micha Alt <mail@lean-stack.de>
2016-06-22 21:11:44 +0200

    Back to Express. Just as a base web framework.

--- server.js
+++ server.js
@@ -3,3 +3,12 @@
 
 // NPM dependencies
 import socketio from 'socket.io';
+import express from 'express';
+
+// Modules
+import http from 'http';
+
+// Creating the Express app, the Node server and integrate Socket.io
+let app = express();
+let server = http.createServer(app);
+let io = socketio(server);
