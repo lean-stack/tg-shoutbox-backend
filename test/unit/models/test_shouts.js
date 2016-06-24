98b46dfaad14ac88d7f94c2b7eb945321f354f34
Micha Alt <mail@lean-stack.de>
2016-06-24 09:28:35 +0200

    Adding back the unit tests.

---
+++ test/unit/models/test_shouts.js
@@ -0,0 +1,33 @@
+
+var expect = require('chai').expect;
+
+var Shout = require('../../../app/models/shout').default;
+
+describe('a shout', function() {
+    it('should be invalid if author is empty', function(done) {
+        var shout = new Shout()
+
+        shout.validate(function(err) {
+            expect(err.errors.author).to.exist;
+            done();
+        });
+    });
+
+    it('should be invalid if msg is empty', function(done) {
+        var shout = new Shout()
+
+        shout.validate(function(err) {
+            expect(err.errors.msg).to.exist;
+            done();
+        });
+    });
+
+    it('should have a default date if ommitted', function(done) {
+        var shout = new Shout()
+
+        shout.validate(function(err) {
+            expect(err.errors.date).to.not.exist;
+            done();
+        });
+    });
+});
